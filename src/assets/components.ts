aa4f044be45dbd8f8485c76ef9d93bd25cbae2c3
PoorTable <PoorTable1337@gmail.com>
2023-06-01 00:44:29 +0200

    add base UI view for table with base data

---
+++ src/assets/components.ts
@@ -0,0 +1,19 @@
+export const Components = [
+  {
+    reactNativeName: 'alert',
+    iOSComponent: {
+      name: 'Alert',
+      image: 'https://picsum.photos/200/300'
+    },
+    androidComponent: {
+      name: 'Alert',
+      image: 'https://picsum.photos/200/300'
+    },
+    webComponent: {
+      name: 'Alert',
+      image: 'https://picsum.photos/200/300'
+    },
+    reactNativeImplementation: ['dasdas', 'dasdasfsdgasdf', 'dasdas'],
+    comments: [{ text: '', type: '' }]
+  }
+]
---
+++ src/components/Layout/ComponentsTable.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table'
+
+  export let components
+</script>
+
+<DataTable table$aria-label="People list" style="max-width: 100%;">
+  <Head>
+    <Row>
+      <Cell>reactNativeName</Cell>
+      <Cell>iOSComponent</Cell>
+      <Cell>androidComponent</Cell>
+      <Cell>webComponent</Cell>
+      <Cell>reactNativeImplementation</Cell>
+      <Cell>comments</Cell>
+    </Row>
+  </Head>
+  <Body>
+    {#each components as el}
+      <Row>
+        <Cell>{el.reactNativeName}</Cell>
+        <Cell>{el.iOSComponent.name}</Cell>
+        <Cell>{el.androidComponent.name}</Cell>
+        <Cell>{el.webComponent.name}</Cell>
+        <Cell>{el.reactNativeImplementation.join('\n')}</Cell>
+        <Cell>{el.comments.map(comp => comp.text)}</Cell>
+      </Row>
+    {/each}
+  </Body>
+</DataTable>
