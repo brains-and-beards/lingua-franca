0075e7ee0c83305aad3e555d24085b79271b2adb
PoorTable <PoorTable1337@gmail.com>
2023-05-31 11:21:45 +0200

    install netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
+  import tailwind from '@astrojs/tailwind'
 
-import tailwind from '@astrojs/tailwind'
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte(), tailwind()]
-})
+  // Enable Svelte to support Svelte components.
+  integrations: [svelte(), tailwind()],
+  output: "server",
+  adapter: netlify()
+});
