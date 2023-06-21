51b667081be0f298cc47d9a6a0c6f3492ac78231
Ilya Kushner <60060961+PoorTable@users.noreply.github.com>
2023-06-21 18:18:36 +0200

    Redesign page UI (#6)
    * set up environments, tailwind, prettier and add LayoutWrapperComponent
    
    * set up tailwind
    
    * set up prettier
    
    * set up ESLint (with website config)
    
    * adjust the gitignore
    
    * add aliases, adjust ESLint config and add assets folder
    
    * add base UI view for table with base data
    
    * add type for components array and adjust table columns
    
    * adjust naming for columns
    
    * adjust styling, columns, and complete styling
    
    * import data from table (modal dialogues)
    
    * import data from table (non-modal dialogues)
    
    * adjust styling for iOS components images
    
    * import data from table (navigation)
    
    * import data from table (Buttons)
    
    * import data from table (controls)
    
    * import data from table (controls)
    
    * import data from table (lists)
    
    * import data from table (progress and push) (final)
    
    * prettify ComponentsTable.svelte and remove unused file
    
    * separate components into different tables
    
    * use real test from the table, and not mocked one
    
    * re-design the table
    
    * re-design the table
    
    * fix image url
    
    * fix image import
    
    * implement an idea with sidebar and header
    
    * fix build process
    
    * fix pages
    
    * resolve conflicts, and fix linter(prettier) issues
    
    * fix build issues
    
    * fix linter and prettier issues, and update first page
    
    * prettify code
    
    ---------
    
    Co-authored-by: IlyaKushn <ilya.kushner@dektry.com>

---
+++ src/assets/sidebar.ts
@@ -0,0 +1,15 @@
+export const SIDEBAR = [
+  { text: 'Dialogs', header: true },
+  { text: 'Modal', link: '/dialogs/modalDialogs' },
+  { text: 'Non-modal', link: '/dialogs/nonModalDialogs' },
+
+  { text: 'Lower-level Views', header: true },
+  { text: 'Bars', link: '/lowerLevelViews/bar' },
+  { text: 'Buttons, inputs and other components', header: true },
+  { text: 'Buttons', link: '/utility/buttons' },
+  { text: 'Input Fields', link: '/utility/inputFields' },
+  { text: 'Other components', header: true },
+  { text: 'Lists', link: '/other/lists' },
+  { text: 'Progress indicators', link: '/other/progressIndicators' },
+  { text: 'External (out-of-app) push notifications', link: '/other/externalPushNotifications' }
+]
